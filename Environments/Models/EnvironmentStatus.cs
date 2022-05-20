c98e515fad38b14c874e7a1cd03470b56c139cf0
illia_shapovalov <ilya.shapovalov.1989@gmail.com>
2022-05-20 20:50:40 +0300

    Added models, added connection string

---
+++ Environments/Models/EnvironmentStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Environments.Models
+{
+    public class EnvironmentStatus
+    {
+        public int Id { get; set; }
+        public string  EnvironmentName { get; set; }
+        public string Credentials { get; set; }
+        public string EmployeeName { get; set; }
+        public Stack Stack { get; set; }
+        public string IsBusy { get; set; }
+        public List<Stack> EnvIsApplicableFor { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ Environments/Models/Stack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+
+namespace Environments.Models
+{
+    public class Stack
+    {
+        public int Id { get; set; }
+        public string StackName { get; set; }
+    }
+}
